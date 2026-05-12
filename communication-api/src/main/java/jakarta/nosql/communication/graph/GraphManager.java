1b3fcf58a4af7094b7d4c4ffeee653cbd310aee0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-12 19:07:38 +0100

    feat: add initial GraphManager class to graph communication package

--- communication-api/src/main/java/jakarta/nosql/communication/graph/GraphManager.java
+++ communication-api/src/main/java/jakarta/nosql/communication/graph/GraphManager.java
@@ -1,4 +1,17 @@
 package jakarta.nosql.communication.graph;
 
 public class GraphManager {
+
+    Vertex put(Vertex vertex);
+
+    Edge put(Edge edge);
+
+    Optional<Vertex> findVertexById(Object id);
+
+    Optional<Edge> findEdgeById(Object id);
+
+    void deleteVertexById(Object id);
+
+    void deleteEdgeById(Object id);
+
 }

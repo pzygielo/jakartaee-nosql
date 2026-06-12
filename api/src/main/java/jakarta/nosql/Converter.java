35e62bee7fc18786682e81449b286ce424da194e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-12 20:05:39 +0100

    feat: define type target and runtime retention for Converter annotation

--- api/src/main/java/jakarta/nosql/Converter.java
+++ api/src/main/java/jakarta/nosql/Converter.java
@@ -15,6 +15,14 @@
  */
 package jakarta.nosql;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
 public @interface Converter {
 
     /**
